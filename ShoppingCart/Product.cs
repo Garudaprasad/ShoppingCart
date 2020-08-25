8ef3ab0624d9937eae6fd9388fdea96381dde7dd
Garudaprasad K <garudaprasad.k@gmail.com>
2020-08-25 08:42:51 +0530

    Adding Product class
    Public class which is used in overall application

---
+++ ShoppingCart/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace ShoppingCart
+{
+    public class Product
+    {
+        private string _productName;
+        private decimal _price;
+
+        public string ProductName
+        {
+            get
+            {
+                return _productName;
+            }
+        }
+
+        public decimal ProductPrice
+        {
+            get { return _price; }
+        }
+
+        public Product(string productName, decimal price)
+        {
+            this._productName = productName;
+            this._price = price;
+        }
+    }
+}
