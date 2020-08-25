80a8b7345915d9900857f106609bcde7f43588d1
Garudaprasad K <garudaprasad.k@gmail.com>
2020-08-25 08:46:07 +0530

    Creating Store Class
    The store Class contains the Products List which needs to be initialized during its creation i.e. on constructor call.

---
+++ ShoppingCart/Store.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingCart
+{
+    public class Store
+    {
+        private List<Product> _products;
+
+        public List<Product> AllProducts
+        {
+            get { return _products; }
+        }
+
+        private Store()
+        {
+            _products = new List<Product>();
+        }
+    }
+}
