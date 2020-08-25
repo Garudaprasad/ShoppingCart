c15d7b3fa949611e9421e36b9a4b2a01a39a0a71
Garudaprasad K <garudaprasad.k@gmail.com>
2020-08-25 09:28:44 +0530

    Adding Public class Cart.cs
    As a single store can have multiple carts hence created public class.
    Which contains individual list of purchases
    initialises the purchase during the Constructor calls

---
+++ ShoppingCart/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ProblemStatement1
+{
+    public class Cart
+    {
+        private List<Purchase> _purchases;        
+
+        public List<Purchase> AllPurchases
+        {
+            get { return _purchases; }
+        }
+        
+        public Cart()
+        {
+            _purchases = new List<Purchase>();
+        }
+    }
+}
---
+++ ShoppingCartTests/CartTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemStatement1.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            new PrivateType(typeof(Store)).SetStaticField("_instance", null);
+        }
+    }
+}
