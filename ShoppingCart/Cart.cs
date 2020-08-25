1ee4283e66189d052d2c5c4beaf134ec2f5a647a
Garudaprasad K <garudaprasad.k@gmail.com>
2020-08-25 09:35:45 +0530

    Add New purchase methods with test cases

--- ShoppingCart/Cart.cs
+++ ShoppingCart/Cart.cs
@@ -10,10 +10,40 @@ namespace ProblemStatement1
         {
             get { return _purchases; }
         }
-        
+
         public Cart()
         {
             _purchases = new List<Purchase>();
+        }
+
+        public bool AddPurchaseToCart(string productName, int units)
+        {
+            if (Store.Instance.AllProducts.Count > 0 && units != 0)
+            {
+                Purchase purchase = _purchases.Find(u => u.ProductName.ToLower().Equals(productName.ToLower()));
+                if (purchase == null)
+                {
+                    Product prod = Store.Instance.AllProducts.Find(u => u.ProductName.ToLower().Equals(productName.ToLower()));
+                    if (prod != null)
+                    {
+                        _purchases.Add(new Purchase(prod, units));
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    purchase.Quantity += units;
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- ShoppingCartTests/CartTests.cs
+++ ShoppingCartTests/CartTests.cs
@@ -11,5 +11,42 @@ namespace ProblemStatement1.Tests
         {
             new PrivateType(typeof(Store)).SetStaticField("_instance", null);
         }
+
+        [TestMethod()]
+        public void AddPurchaseToCart_NoProducts_ProductAddition_Failed()
+        {
+            Cart cart = new Cart();
+            Assert.AreEqual(cart.AddPurchaseToCart("A", 12), false);
+            Assert.AreEqual(cart.AllPurchases.Count, 0);
+        }
+
+        [TestMethod()]
+        public void AddPurchaseToCart_WithProducts_ProductAdded_Success()
+        {
+            Store.Instance.AddProductToStore("A", 12);
+            Cart cart = new Cart();
+            Assert.AreEqual(cart.AddPurchaseToCart("A", 10), true);
+
+            Assert.AreEqual(cart.AllPurchases.Count, 1);
+            Assert.AreEqual(cart.AllPurchases[0].ProductName, "A");
+            Assert.AreEqual(cart.AllPurchases[0].Quantity, 10);
+            Assert.AreEqual(cart.AllPurchases[0].Prod.ProductPrice, 12);
+        }
+
+        [TestMethod()]
+        public void AddPurchaseToCart_WithPurchase_AddQuantity_Success()
+        {
+            Store.Instance.AddProductToStore("A", 12);
+            Cart cart = new Cart();
+            Assert.AreEqual(cart.AddPurchaseToCart("A", 10), true);
+
+            Assert.AreEqual(cart.AllPurchases.Count, 1);
+            Assert.AreEqual(cart.AllPurchases[0].ProductName, "A");
+            Assert.AreEqual(cart.AllPurchases[0].Quantity, 10);
+            Assert.AreEqual(cart.AllPurchases[0].Prod.ProductPrice, 12);
+
+            cart.AddPurchaseToCart("A", 10);
+            Assert.AreEqual(cart.AllPurchases[0].Quantity, 20);
+        }
     }
 }
