c297fa17a966938aa26bcfceeee7f8aaccf374bd
Garudaprasad K <garudaprasad.k@gmail.com>
2020-08-25 09:49:57 +0530

    Interface for Promotions
    Which needs to have one method for sure i.e. Bill

---
+++ ShoppingCart/IPromotion.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemStatement1
+{
+    public interface IPromotion
+    {
+        void Bill(List<Purchase> purchases, ref StringBuilder billAsString, ref decimal total);
+    }
+}
